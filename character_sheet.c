eb6241e4d92aa54fccfb33d7c4213d95241ecb8a
scanlona <59579777+scanlona@users.noreply.github.com>
2025-09-28 12:55:36 -0700

    Implement Classes, Features to Accept Name and Class, Output Character Sheet
    Implemented new features to accept various user inputs at appropriate times to keep the character's name and class, fully customizable to the user. At any time, the user may type 'character sheet' to immediately produce a character sheet constructed as is with the information that has been given so far. Also, the 'exit' function will reliably work from anywhere in the program.

--- character_sheet.c
+++ character_sheet.c
@@ -1,15 +1,83 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 char userInput[100];
+
+bool nameFlag = false;
+bool classFlag = false;
+bool exitFlag = false;
+char characterName[100] = "N/A";
+char characterClass[100] = "N/A";
+
+void checkForPrintCharacterSheet() {
+    if (strcmp(userInput, "character sheet") == 0) {
+        printf("NAME: %s\nCLASS: %s\n\n", characterName, characterClass);
+    }
+}
+
+void getCharacterName() {
+    while (nameFlag == false) {
+        printf("Enter your character's name!\n\n");
+        printf(">");
+        fgets(userInput, 100, stdin);
+        userInput[strcspn(userInput, "\n")] = 0;
+        if (strcmp(userInput, "character sheet") != 0 && strcmp(userInput, "exit") != 0) {
+            strcpy(characterName, userInput);
+            printf("You entered: %s\n", characterName);
+            printf("Your character's name rules!\n\n");
+        }
+        checkForPrintCharacterSheet();
+        if (strcmp(userInput, "exit") == 0) {
+            exitFlag = true;
+            break;
+        }
+        if (strcmp(characterName, "N/A") != 0) {
+            nameFlag = true;
+        }
+    }
+}
+
+void getCharacterClass() {
+    while (classFlag == false) {
+        printf("Enter your character's class!\n\n");
+        printf(">");
+        fgets(userInput, 100, stdin);
+        userInput[strcspn(userInput, "\n")] = 0;
+        if (strcmp(userInput, "character sheet") != 0 && strcmp(userInput, "exit") != 0) {
+            strcpy(characterClass, userInput);
+            printf("You entered: %s\n", characterClass);
+            printf("Sweet!\n\n");
+        }
+        checkForPrintCharacterSheet();
+        if (strcmp(userInput, "exit") == 0) {
+            exitFlag = true;
+            break;
+        }
+        if (strcmp(characterClass, "N/A") != 0) {
+            classFlag = true;
+        }
+    }
+}
 
 int main() {
     printf("Starting program...\n");
     printf("(Enter 'exit' anytime to exit program)\n\n");
     printf("Initializing character sheet...\n\n");
     while (1 != 0) {
+        getCharacterName();
+        if (exitFlag == true) {
+            break;
+        }
+        getCharacterClass();
+        if (exitFlag == true) {
+            break;
+        }
+        printf(">");
         fgets(userInput, 100, stdin);
         userInput[strcspn(userInput, "\n")] = 0;
+        checkForPrintCharacterSheet();
         if (strcmp(userInput, "exit") == 0) {
             break;
         }
